0d445a20b9f68108a9dfb6f18b9626494a130ac8
Trevor Cambron <trevorwcambron@gmail.com>
2024-12-03 15:03:29 -0500

    data extracted

---
+++ 01.10data_extraction.py
@@ -0,0 +1,27 @@
+##### Extract Abstracts and Associated Data ######################
+#     The goal of this script is to use the habanero package
+#     to access CrossRef's API and extract abstracts published
+#     since 2013 and additional meta-data 
+
+#### Imports #####################################################
+from habanero import Crossref
+import pandas as pd
+import json
+
+#### Helper Functions ############################################
+cr = Crossref()
+
+#### Main ########################################################
+years_of_interest = range(2017,2024)
+
+for year in years_of_interest:
+    res = cr.works(query = "climate warming CO2",
+                    cursor = "*", 
+                    limit = 100, 
+                    cursor_max=100000,
+                    filter = {'type': 'journal-article', 'from-pub-date': str(year), 'until-pub-date': str(year)},
+                    progress_bar=True
+                )
+    file_name = f'climate_articles_{year}.json'
+    with open(file_name, 'w') as f:
+        json.dump(res, f, indent=4)
